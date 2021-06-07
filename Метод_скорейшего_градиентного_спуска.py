8788c75756757483ce46ddad21d75a105a25ebad
mirolyubovaalyona <niroliubova.aa@students.dvfu.ru>
2021-06-08 01:18:29 +1000

    +1

--- Метод_скорейшего_градиентного_спуска.py
+++ Метод_скорейшего_градиентного_спуска.py
@@ -2,12 +2,10 @@ import numpy as np
 
 def f(n, a, b, e, xt):
     x = np.array(np.zeros(n))
-    x=[0.3, -0.05, -0.2, 0.3]
     t=False
     k=0
     while(t==False):
         r=(a.dot(x)-b)
-        print('r, ', r)
         u=(r*(a.dot(r.dot(a.transpose()))))/((a.dot(r.dot(a.transpose())))*(a.dot(r.dot(a.transpose()))))
         x=x-u*a.transpose().dot(r)
         k+=1
@@ -24,15 +22,12 @@ print("z")
 
 e=0.001
 
-a= [[8, -1, -2, 0],
-    [0, 10, -2, 2],
-    [-1, 0, 6, 2],
-[3, -1, 2, 12]]
+a= [[20, 2, 3, 7],
+[1, 12, -2, -5],
+[5, -3, 13, 0],
+[0, 0, -3, 15]]
 
-
-
-
-b=[-2.3, 0.5, 1.2, -3.7]
+b=[5, 4, -3, 7]
 
 
 n=len(b)
