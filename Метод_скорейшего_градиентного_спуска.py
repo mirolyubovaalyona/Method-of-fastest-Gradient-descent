2245b096b2c9060d41f87cb625d99e7d69ebabc0
mirolyubovaalyona <niroliubova.aa@students.dvfu.ru>
2021-06-08 02:40:44 +1000

    end

--- Метод_скорейшего_градиентного_спуска.py
+++ Метод_скорейшего_градиентного_спуска.py
@@ -6,8 +6,9 @@ def f(n, a, b, e, xt):
     k=0
     while(t==False):
         r=(a.dot(x)-b)
-        u=(r*(a.dot(r.dot(a.transpose()))))/((a.dot(r.dot(a.transpose())))*(a.dot(r.dot(a.transpose()))))
-        x=x-u*a.transpose().dot(r)
+        u=(r*r)/(a.dot(r)*r)
+        #grad f(x)=2(Ax-b)
+        x=x-u*r
         k+=1
         print(x)
         t=True
@@ -18,16 +19,20 @@ def f(n, a, b, e, xt):
     return x
 
 
-print("z")
+
 
 e=0.001
 
-a= [[20, 2, 3, 7],
+a=[[20, 2, 3, 7],
 [1, 12, -2, -5],
 [5, -3, 13, 0],
 [0, 0, -3, 15]]
 
+
+
+
 b=[5, 4, -3, 7]
+
 
 
 n=len(b)
